a06100c4c58b687f8838f4ff1e2a1820e9726581
Peter Ivanov <peter@microweber.com>
2020-04-10 22:04:35 +0300

    Update download.php

--- views/download.php
+++ views/download.php
@@ -30,7 +30,7 @@ if (!isset($latest_plugin_version)) {
         <form method="POST">
             <div class="row">
                 <div class="col-md-4">
-                    <h2>Microweber version $</h2>
+                    <h2>Microweber version</h2>
 
                     <?php if (!$current_version): ?>
                         <h5>You dont have any downloaded version</h5>
