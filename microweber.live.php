8f8de06c628394f50227ba5bcde7feeee5c2d110
Peter Ivanov <peter@microweber.com>
2021-06-24 15:53:08 +0300

    update

--- microweber.live.php
+++ microweber.live.php
@@ -1,6 +1,11 @@
 <?php
 
 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include('/usr/local/cpanel/php/cpanel.php');
 
 require_once(__DIR__ . '/lib/MicroweberPluginController.php');
