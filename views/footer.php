f68564b234e33feba2860403ea503c2f31f389ae
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-09 17:39:08 +0300

    update

--- views/footer.php
+++ views/footer.php
@@ -7,31 +7,19 @@
         });
     });
 
-
     $(document).ready(function () {
-
         $('.js-action-update-installations').on('click', function () {
-
             var el = $("#domains-ajax-result");
             if (el) {
-
                 var txt;
                 var r = confirm("Are you sure you want to update existing installations with the new settings?");
                 if (r == true) {
-
                     el[0].scrollIntoView({behavior: 'smooth'})
-
                     el.html('Updating installations, please wait...');
                     el.load("?ajax_view=update_installs_with_new_settings", function () {
-
-
                     });
                 }
-
             }
-
         });
     });
-
-
 </script>
--- whm/index.cgi
+++ whm/index.cgi
@@ -1,5 +1,4 @@
 #!/usr/local/cpanel/3rdparty/bin/php-cgi
-
 <?php
 include_once('/usr/local/cpanel/microweber/whm/admin.php');
 ?>
