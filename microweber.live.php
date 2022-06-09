6b72964bc0628a5070ba63de9043d2da363b6e9d
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-09 17:47:34 +0300

    update

--- microweber.live.php
+++ microweber.live.php
@@ -1,1 +1,8 @@
-hi
+<?php
+use App\Kernel;
+
+require_once dirname(__DIR__).'/plugin/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
--- whm/admin.php
+++ whm/admin.php
@@ -4,3 +4,13 @@ WHM::header('Microweber Settings', 1, 1);
 
 WHM::footer();*/
 ?>
+
+<?php
+use App\Kernel;
+
+require_once dirname(__DIR__).'/plugin/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
+?>
