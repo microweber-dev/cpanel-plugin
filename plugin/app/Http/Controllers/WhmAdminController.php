15e0b27a79d1600db7eac503a9bdb547ac8a72b3
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-09 18:07:47 +0300

    update

---
+++ plugin/app/Http/Controllers/WhmAdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WhmAdminController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        dump($request);
+
+        return view('welcome');
+    }
+}
--- whm/admin.php
+++ whm/admin.php
@@ -1,16 +1,55 @@
 <?php
-/*require_once('/usr/local/cpanel/php/WHM.php');
-WHM::header('Microweber Settings', 1, 1);
 
-WHM::footer();*/
-?>
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-<?php
-use App\Kernel;
+define('LARAVEL_START', microtime(true));
 
-require_once dirname(__DIR__).'/plugin/vendor/autoload_runtime.php';
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
-?>
+if (file_exists($maintenance = dirname(__DIR__).'/plugin/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require dirname(__DIR__).'/plugin/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once dirname(__DIR__).'/plugin/bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
