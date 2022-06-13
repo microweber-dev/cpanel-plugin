ae340951ecb474b98a29def7b697d182e64633d7
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-13 12:35:06 +0300

    update

--- plugin/app/Http/Livewire/WhmVersions.php
+++ plugin/app/Http/Livewire/WhmVersions.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class WhmVersions extends Component
 {
+    public $currentVersionOfApp = 0;
+    public $latestVersionOfApp = 0;
+    public $latestDownloadDateOfApp = 0;
+    public $availableTemplatesOfApp = [
+        'new-world'=>[
+            'name'=>'New World',
+            'version'=>'1.2',
+        ]
+    ];
+
     public function render()
     {
         return view('livewire.whm.versions');
--- plugin/resources/views/livewire/whm/versions.blade.php
+++ plugin/resources/views/livewire/whm/versions.blade.php
@@ -1,3 +1,21 @@
 <div>
-    versions
+    <h5>Microweber version</h5>
+    <span>Your version is: </span> <b>{{$currentVersionOfApp}}</b> <br>
+    <span>Latest version is:</span> <b>{{$latestVersionOfApp}}</b> <br>
+    <span>Latest download date:</span> <b>{{$latestDownloadDateOfApp}}</b><br>
+    <br>
+
+    <h5>Available Templates ({{count($availableTemplatesOfApp)}})</h5>
+    @if(!empty($availableTemplatesOfApp))
+    @foreach($availableTemplatesOfApp as $template)
+    {{$template['name']}} ({{$template['version']}})
+    @endforeach
+    @else
+        No templates installed
+    @endif
+
+    <br>
+    <br>
+    <button class="btn btn-outline-success" wire:click="checkForUpdate()" style="width:200px;">Check for updates</button>
+
 </div>
