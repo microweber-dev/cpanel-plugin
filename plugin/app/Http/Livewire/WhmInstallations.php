1297e1ab715f0378fc8916408c3b0c1ddbbd9143
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-14 14:54:00 +0300

    update

--- plugin/app/Http/Livewire/WhmInstallations.php
+++ plugin/app/Http/Livewire/WhmInstallations.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Console\Commands\AppInstallationsSync;
 use Livewire\Component;
 
 class WhmInstallations extends Component
 {
+    protected $listeners = ['refreshComponent' => '$refresh'];
+    
     public function render()
     {
         return view('livewire.whm.installations');
+    }
+
+    public function scan()
+    {
+        dispatch(new AppInstallationsSync());
+
+        $this->emit('refreshComponent');
     }
 }
--- plugin/resources/views/livewire/whm/installations.blade.php
+++ plugin/resources/views/livewire/whm/installations.blade.php
@@ -1,3 +1,14 @@
 <div>
     <livewire:whm-app-installations-table />
+
+
+    <br />
+    <button class="btn btn-sm btn-outline-success" wire:click="scan()" wire:loading.attr="disabled">
+        Scan for new installations
+    </button>
+
+    <div wire:loading wire:target="scan">
+        Scanning ...
+    </div>
+
 </div>
