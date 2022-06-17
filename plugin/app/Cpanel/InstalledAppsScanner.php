776c2e77f8ba3cded4940c6310163034589a4f4e
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-17 12:53:50 +0300

    Update InstalledAppsScanner.php

--- plugin/app/Cpanel/InstalledAppsScanner.php
+++ plugin/app/Cpanel/InstalledAppsScanner.php
@@ -3,7 +3,6 @@
 namespace App\Cpanel;
 
 use App\MicroweberVersionsManager;
-use MicroweberPackages\SharedServerScripts\MicroweberAppPathHelper;
 use MicroweberPackages\SharedServerScripts\MicroweberInstallationsRecursiveScanner;
 
 class InstalledAppsScanner
