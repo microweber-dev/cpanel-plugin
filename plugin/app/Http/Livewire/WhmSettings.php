cfba7eca30a4ec7048f87922157e5148e9e55377
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-28 20:18:18 +0300

    Update WhmSettings.php

--- plugin/app/Http/Livewire/WhmSettings.php
+++ plugin/app/Http/Livewire/WhmSettings.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Option;
 use Livewire\Component;
 use MicroweberPackages\SharedServerScripts\MicroweberAppPathHelper;
+use MicroweberPackages\SharedServerScripts\MicroweberWhmcsConnector;
 
 class WhmSettings extends Component
 {
@@ -18,7 +19,17 @@ class WhmSettings extends Component
     {
         if (!empty($this->state)) {
             foreach ($this->state as $key=>$value) {
-                Option::updateOption($key, $value, 'settings');
+                Option::updateOption($key, trim($value), 'settings');
+            }
+
+            $whmcsUrl = Option::getOption('whmcs_url', 'settings');
+            if (isset($this->state['whmcs_url'])) {
+                if ($this->state['whmcs_url'] != $whmcsUrl) {
+                    $whmcs = new MicroweberWhmcsConnector();
+                    $whmcs->setPath(config('whm-cpanel.sharedPaths.app'));
+                    $whmcs->setUrl($whmcsUrl);
+                    $whmcs->applySettingsToPath();
+                }
             }
         }
 
