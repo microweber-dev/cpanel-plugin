ec59eb23f31dfb610e38dd463423d762d72260b9
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-28 18:59:46 +0300

    Update WhmAppInstallationsTable.php

--- plugin/app/Http/Livewire/WhmAppInstallationsTable.php
+++ plugin/app/Http/Livewire/WhmAppInstallationsTable.php
@@ -33,6 +33,9 @@ class WhmAppInstallationsTable extends DataTableComponent
          //   Column::make("Id", "id")->sortable(),
             ScreenshotColumn::make("Screenshot", "screenshot")
                 ->location(function($row) {
+                if (!empty($row->screenshot)) {
+                    return $row->screenshot;
+                }
                 return asset('img/no-screenshot.png');
             }),
 
