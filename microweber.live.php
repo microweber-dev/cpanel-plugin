79173c867ebd6017c3041f26048950ca2daecaad
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-16 13:34:30 +0300

    Update microweber.live.php

--- microweber.live.php
+++ microweber.live.php
@@ -1,5 +1,20 @@
 <?php
 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include('/usr/local/cpanel/php/cpanel.php');
+
+
+$cpanel = new CPANEL();
+echo $cpanel->header();
+echo $cpanel->footer();
+$cpanel->end();
+return;
+
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
