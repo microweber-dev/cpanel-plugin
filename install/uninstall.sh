4196a0d4f6efa6f63fd8c6d3605bfb9d8043f43a
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-17 15:30:23 +0300

    Update uninstall.sh

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -9,11 +9,6 @@ if [ "$username" != "root" ]; then
     echo "Please run this script as root";
 fi
 
-/usr/local/cpanel/scripts/uninstall_plugin /usr/local/cpanel/microweber/install/mw-plugin
-/usr/local/cpanel/bin/unregister_appconfig /usr/local/cpanel/microweber/install/microweber.conf
-
-/usr/local/cpanel/bin/manage_hooks delete script /usr/local/cpanel/microweber/hooks/mw_hooks.php
-
 /bin/rm -rf /usr/local/cpanel/whostmgr/docroot/cgi/3rdparty/microweber
 /bin/rm -rf /usr/local/cpanel/base/frontend/paper_lantern/microweber
 /bin/rm -rf /usr/local/cpanel/base/frontend/jupiter/microweber
@@ -22,3 +17,7 @@ fi
 /bin/rm -rf /var/cpanel/microweber
 /bin/rm -rf /usr/local/cpanel/whostmgr/docroot/cgi/microweber
 /bin/rm -rf /usr/local/cpanel/whostmgr/docroot/templates/microweber
+
+/usr/local/cpanel/scripts/uninstall_plugin /usr/local/cpanel/microweber/install/mw-plugin
+/usr/local/cpanel/bin/unregister_appconfig /usr/local/cpanel/microweber/install/microweber.conf
+/usr/local/cpanel/bin/manage_hooks delete script /usr/local/cpanel/microweber/hooks/mw_hooks.php
