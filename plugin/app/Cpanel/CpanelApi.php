1af2f8c3ee0bf117e496298759516842ff6463b7
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-17 13:16:32 +0300

    Update CpanelApi.php

--- plugin/app/Cpanel/CpanelApi.php
+++ plugin/app/Cpanel/CpanelApi.php
@@ -1,10 +1,34 @@
 <?php
-
 namespace App\Cpanel;
-
 
 class CpanelApi
 {
+    public function getHostingDetailsByDomainName($domainName)
+    {
+        $details = [];
+
+        $allDomains = $this->getAllDomains();
+
+        if (!empty($allDomains)) {
+            foreach ($allDomains as $domain) {
+                if ($domain['domain'] == $domainName) {
+                    $details = $domain;
+                }
+            }
+        }
+
+        return $details;
+    }
+
+    public function getAllDomains()
+    {
+        $domainRequest = $_SERVER['cpanelApi']->uapi('DomainInfo', 'domains_data', array('format' => 'hash'));
+        $domainRequest = $domainRequest['cpanelresult']['result']['data'];
+        $domains = array_merge(array($domainRequest['main_domain']), $domainRequest['addon_domains'], $domainRequest['sub_domains']);
+
+        return $domains;
+    }
+
     public function getUsername()
     {
         $username = $_SERVER['cpanelApi']->exec('<cpanel print="$user">');
