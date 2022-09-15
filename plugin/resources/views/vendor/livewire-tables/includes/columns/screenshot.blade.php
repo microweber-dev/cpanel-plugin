e218df1fc4104fccb7aa9b9290a62b55ff8a662a
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-15 13:37:24 +0300

    dont remove livewire tables

---
+++ plugin/resources/views/vendor/livewire-tables/includes/columns/screenshot.blade.php
@@ -0,0 +1,3 @@
+<div style="max-height:100px;text-align:center;overflow: hidden;">
+    <img src="{{ $path }}" {!! count($attributes) ? $column->arrayToAttributes($attributes) : '' !!} style="width:130px;">
+</div>
