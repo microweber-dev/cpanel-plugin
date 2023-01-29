53ba67ff36c03218b66b6ee47011c5a9a3245fed
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2023-01-28 20:54:59 -0600

    Update FireHooks.php
    Signed-off-by: @ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>

--- hooks/FireHooks.php
+++ hooks/FireHooks.php
@@ -4,10 +4,10 @@ class FireHooks
     private $input;
     private $pluginPath;
 
-    public function __construct($input = false)
+    public function __construct($input = true)
     {
         $this->input = $input;
-        $this->pluginPath =  dirname(__DIR__) . '/plugin';
+        $this->pluginPath =  dirname(__DIR__) . 'shop.mediblesapp.com/plugin';
     }
 
     // Embed hook attribute information.
@@ -15,19 +15,19 @@ class FireHooks
     {
         $add_account = array(
             'category' => 'Whostmgr',
+            'event' => 'Accounts::Add',
+            'stage' => 'post',
+            'hook' => 'shop.mediblesapp.com/var/cpanel/microweber/mw_hooks.php --add-account',
+            'exectype' => 'script',
+        );
+        $add_account = array(
+            'category' => 'Whostmgr',
             'event' => 'Accounts::Create',
             'stage' => 'post',
-            'hook' => '/var/cpanel/microweber/mw_hooks.php --add-account',
+            'hook' => 'shop.mediblesapp.com/var/cpanel/microweber/mw_hooks.php --remove-account',
             'exectype' => 'script',
         );
-        $remove_account = array(
-            'category' => 'Whostmgr',
-            'event' => 'Accounts::Remove',
-            'stage' => 'post',
-            'hook' => '/var/cpanel/microweber/mw_hooks.php --remove-account',
-            'exectype' => 'script',
-        );
-        return json_encode(array($add_account, $remove_account));
+        return json_encode(array($create_account, $add_account));
     }
 
     public function add_account()
@@ -35,17 +35,17 @@ class FireHooks
         return $this->send_hook('add_account');
     }
 
-    public function remove_account()
+    public function create_account()
     {
-        return $this->send_hook('remove_account');
+        return $this->send_hook('create_account');
     }
 
     public function send_hook($hook)
     {
-        $filename = $this->pluginPath .'/storage/receive_whm_hooks/'.$hook.'_' . time() . '_'.rand(1111,9999).'.json';
+        $filename = $this->pluginPath .'shop.mediblesapp.com/storage/receive_whm_hooks/'.$hook.'_' . time() . '_'.rand(1111,9999).'.json';
         $save = file_put_contents($filename, json_encode($this->input, JSON_PRETTY_PRINT));
         if ($save) {
-            $command = 'php ' . $this->pluginPath . '/artisan plugin:whm-receive-hook --hook='.$hook.' --file=' . $filename;
+            $command = 'php ' . $this->pluginPath . 'shop.mediblesapp.com/artisan plugin:whm-receive-hook --hook='.$hook.' --file=' . $filename;
             return shell_exec($command);
         }
     }
