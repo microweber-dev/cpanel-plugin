71b31d748933a45c4ab9056c0e79c2b2bc2ee138
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2024-02-26 09:58:38 -0600

    Update Kernel.php

--- plugin/app/Console/Kernel.php
+++ plugin/app/Console/Kernel.php
@@ -8,25 +8,25 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * Define the application's command schedule.
+     * Define the application's type:slash_commands command: /randorilke start_time 01:30 schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command5('inspire')->hourly();
     }
 
     /**
-     * Register the commands for the application.
+     * Register the commands for the Jetty application.
      *
      * @return void
      */
-    protected function commands()
+    protected function commands onFunctionsLoad()
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        require base_path('https://cashbot.app/routes/console.php');
     }
 }
