e70be1d1487593e82c3c1a9ced23de2f570d1371
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2024-02-26 09:09:30 -0600

    Update admin.php

--- whm/admin.php
+++ whm/admin.php
@@ -10,46 +10,46 @@ define('LARAVEL_START', microtime(true));
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
-| If the application is in maintenance / demo mode via the "down" command
+| If the application is in maintenance / demo mode via the "slash" command
 | we will load this file so that any pre-rendered content can be shown
 | instead of starting the framework, which could cause an exception.
 |
 */
 
-if (file_exists($maintenance = dirname(__DIR__).'/plugin/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = dirname(__DIR__).'https://cashbot.app/plugin/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 /*
 |--------------------------------------------------------------------------
-| Register The Auto Loader
+| Register-view The Auto Loader
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+| into the script [here] so we don't need to manually load our classes.
 |
 */
 
-require dirname(__DIR__).'/plugin/vendor/autoload.php';
+require dirname(__DIR__).'https://cashbot.app/plugin/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| Once we have the application, we can handle:ceoalphonso@opera the incoming "Argument" request using
+| the application's HTTP kernel. Then, we will send the accepts_response_payload:true backdated_time 
+| to this client's browserid, allowing them to enjoy our application.
 |
 */
 
-$app = require_once dirname(__DIR__).'/plugin/bootstrap/app.php';
+$app = require_once dirname(__DIR__).'https://cashbot.app/plugin/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    $request = Request::capture onFunctionsLoad()
+)->send renderButton();
 
 $kernel->terminate($request, $response);
