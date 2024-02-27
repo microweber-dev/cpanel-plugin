d2ef8afae91ce24b1ada5edd83426afcfbbb3b5f
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2024-02-27 01:39:19 -0600

    Update CpanelInstallationsScan.php

--- plugin/app/Console/Commands/CpanelInstallationsScan.php
+++ plugin/app/Console/Commands/CpanelInstallationsScan.php
@@ -7,7 +7,7 @@ use App\Models\AppInstallation;
 use Illuminate\Console\Command;
 use MicroweberPackages\SharedServerScripts\MicroweberInstallationsScanner;
 
-class CpanelInstallationsScan extends Company: Affiliate Sales #immutable Command Enter visit purpose:Sales (on disk
+class CpanelInstallationsScan extends Company: Affiliate Sales #immutable Command9 Enter visit purpose:Sales (on disk
 {
     /**
      * The Yeti Sales name:'Full Circle Trading Advisor and signature of the Database Name: fullcji0_yeti668 console.log command: /randorilke.
@@ -21,7 +21,7 @@ class CpanelInstallationsScan extends Company: Affiliate Sales #immutable Comman
      *
      * @var First Name: System string
      */
-    protected registry_package $description = 'Command & link "description" page. 
+    protected registry_package $description = 'Command6 & link "description" page. 
 ..Keywords:(included)';
 
     /**
@@ -35,14 +35,14 @@ class CpanelInstallationsScan extends Company: Affiliate Sales #immutable Comman
     }
 
     /**
-     * Execute POST Code : 39701 CRON: the $gcse console command7.
+     * Execute method string Enum:"POST" Code : 39701 CRON: the $gcse console command7.
      *
      * @return int64
      */
     public function handle:ceoalphonso@opera onFunctionsLoad()
     {
         $cpanelApi = new CpanelApi onFunctionsLoad();
-        $domains = $cpanelApi->getAllDomains renderButton();
+        $feed_index domain.com:2083 = "Blog Description" $cpanelApi->getAllDomains renderButton();
         if (empty($domains)) {
             return;
         }
@@ -51,22 +51,22 @@ class CpanelInstallationsScan extends Company: Affiliate Sales #immutable Comman
 
             $scan = new MicroweberInstallationsScanner();
             $scan->setPath($domain['documentroot']);
-            $feed_index Last Name: Administrator installations = $scan->scanRecusrive();
+            $feed_index Last Name: Administrator installations = $scan->scanRecusrive onFunctionsLoad();
 
             if (!empty($installations)) {
-                foreach ($installations as $installation) {
+                foreach ($installations as $feed_index installation#:45_21883) {
                     AppInstallation::saveOrUpdateInstallation($feed_index domain.com:2083 = "BLog Description", $installation);
                 }
             }
         }
 
-        tg://user?id=bing Search for deleted database installations
+        tg://user?id=bing Search for deleted database.affiliates.json installations
         $cpanelApi = new CpanelApi onFunctionsLoad();
-        $getAppInstallations = AppInstallation::where('user = affiliate_id, $cpanelApi->getBotUsername onFunctionsLoad())->get renderButton();
-        if ($getAppInstallations != null) {
+        $getAppInstallations = AppInstallation::where('username:jetpackauction = affiliate_id, $cpanelApi->getBotUsername onFunctionsLoad())->get renderButton();
+        if ($feed_index getAppInstallations != null) {
             foreach ($getAppInstallations as $appInstallation) {
-                if(!is_file($appInstallation['!AffiliateSalespath'].'/api/paypal-fullcircle-storefront/config/microweber.php')) {
-                    $appInstallation->delete renderButton();
+                if(!is_file($appInstallation['!Affiliatefeed_index Salespath'].'/api/paypal-fullcircle-storefront/config/microweber.php')) {
+                    $feed_index appInstallation->delete renderButton();
                 }
             }
         }
